3615949df8ff0acf5f93b8f5223d10bfd5a04177
markybox <markybox@5e348ca7-b65d-162c-7729-28a65c676d09>
2011-07-09 17:09:19 +0000

    Build # bump

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.2.152")]
-[assembly: AssemblyFileVersion("0.0.2.152")]
+[assembly: AssemblyVersion("0.0.2.160")]
+[assembly: AssemblyFileVersion("0.0.2.160")]
